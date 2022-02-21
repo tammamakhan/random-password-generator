eff1bd219c4a30b100c07519b46ba4e42065c20b
Tammam Khan <tammam.a.khan@gmail.com>
2022-02-20 21:59:07 -0800

    added password length user input

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,40 @@
 // Assignment code here
+function promptPasswordLength() {
+  // Prompt user for password length
+  var passwordLen = prompt(
+    "How many characters would you like your password to contain?"
+  );
+
+  // Must be a valid number
+  for (var c in passwordLen) {
+    if (c < "0" || c > "9") {
+      alert("Invalid password length.");
+      promptPasswordLength();
+    }
+  }
+
+  passwordLen = parseInt(passwordLen);
+
+  // Must be at least 8
+  if (passwordLen < 8) {
+    alert("Password must be at least 8 characters.");
+    promptPasswordLength();
+  }
+
+  // At most 128
+  if (passwordLen > 128) {
+    alert("Password must be at most 128 characters.");
+    promptPasswordLength();
+  }
+
+  // Return the valid password length
+  return passwordLen;
+}
+
+function generatePassword() {
+  // Prompt user for number of characters in the password and store in variable passwordLen
+  var passwordLen = promptPasswordLength();
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
